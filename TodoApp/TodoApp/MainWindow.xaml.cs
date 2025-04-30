e927f2c2122f56583957d3cc91f47d2db584e688
tuigildina-17 <tujgildinad@gmail.com>
2025-04-30 20:55:04 +0500

    Add files via upload
    Список задач отдельным проектом

---
+++ TodoApp/TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+using TodoApp.Services;
+
+namespace TodoApp
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+
+        private BindingList<TodoModel> _organizationalTasks;
+        private BindingList<TodoModel> _technicalTasks;
+        private BindingList<TodoModel> _trainingTasks;
+
+        private FileIOService _fileIOService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                var allTasks = _fileIOService.LoadData();
+
+                _organizationalTasks = new BindingList<TodoModel>(allTasks.Where(t => t.Category == "Знакомство с компанией").ToList());
+                _technicalTasks = new BindingList<TodoModel>(allTasks.Where(t => t.Category == "Знакомство с проектом").ToList());
+                _trainingTasks = new BindingList<TodoModel>(allTasks.Where(t => t.Category == "Знакомство с командой").ToList());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+                return;
+            }
+
+            dgOrganizational.ItemsSource = _organizationalTasks;
+            dgTechnical.ItemsSource = _technicalTasks;
+            dgTraining.ItemsSource = _trainingTasks;
+
+            _organizationalTasks.ListChanged += AnyListChanged;
+            _technicalTasks.ListChanged += AnyListChanged;
+            _trainingTasks.ListChanged += AnyListChanged;
+        }
+
+        private void AnyListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemChanged || e.ListChangedType == ListChangedType.ItemDeleted)
+            {
+                try
+                {
+                    var allTasks = _organizationalTasks.Concat(_technicalTasks).Concat(_trainingTasks).ToList();
+                    _fileIOService.SaveData(allTasks);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+
+
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgOrganizational.SelectedItem is TodoModel selectedOrg)
+                _organizationalTasks.Remove(selectedOrg);
+            else if (dgTechnical.SelectedItem is TodoModel selectedTech)
+                _technicalTasks.Remove(selectedTech);
+            else if (dgTraining.SelectedItem is TodoModel selectedTrain)
+                _trainingTasks.Remove(selectedTrain);
+        }
+    }
+}
---
+++ TodoApp/TodoApp/Models/TodoModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TodoApp.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }  // Уникальный идентификатор задачи
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool isDone;
+        private string text;
+        private string category;
+
+        public bool IsDone
+        {
+            get { return isDone; }
+            set
+            {
+                if (isDone == value) return;
+                isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                if (text == value) return;
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set
+            {
+                if (category == value) return;
+                category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

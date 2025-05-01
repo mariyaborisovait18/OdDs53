a0751e1d80466d867ecb1eb1e059ee837fb165bd
litovkaolya18 <olakim710@gmail.com>
2025-05-01 21:21:10 +0500

    сделала базу знаний и добавила кнопки выход вход

---
+++ OnboardingApp/OnboardingApp/ArticleRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ArticleRepository : IArticleRepository
+{
+    private List<Article> _articles;
+
+    public ArticleRepository()
+    {
+        // Здесь вы можете заполнить свои статьи
+        _articles = new List<Article>
+        {
+            new Article { Title = "Инструкция 1", Category = "Инструкции", CreatedDate = DateTime.Now, PdfFilePath = "path/to/document1.pdf" },
+            new Article { Title = "Правила 1", Category = "Правила", CreatedDate = DateTime.Now, PdfFilePath = "path/to/document2.pdf" },
+            // добавьте другие статьи
+        };
+    }
+
+    public IEnumerable<Article> GetAllArticles()
+    {
+        return _articles;
+    }
+}
---
+++ OnboardingApp/OnboardingApp/IArticleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface IArticleRepository
+{
+    IEnumerable<Article> GetAllArticles();
+    // добавьте дополнительные методы по мере необходимости
+}

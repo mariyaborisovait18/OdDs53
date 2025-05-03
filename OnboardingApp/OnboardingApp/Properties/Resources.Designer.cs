f800e80a61a7e56c445c4411260104516ac4da33
Valentina <v4lentinakataeva@yandex.ru>
2025-05-03 12:28:39 +0500

    сохранение файла со списком задач

--- OnboardingApp/OnboardingApp/Properties/Resources.Designer.cs
+++ OnboardingApp/OnboardingApp/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace OnboardingApp.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Byte[].
+        /// </summary>
+        internal static byte[] todoDataList {
+            get {
+                object obj = ResourceManager.GetObject("todoDataList", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
